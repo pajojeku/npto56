fb68767a39f9c15df56c43df1259cd06451bbc7a
pajojeku <84803180+pajojeku@users.noreply.github.com>
2024-04-15 19:56:59 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-<<<<<<< HEAD
+
 Console.WriteLine("To jedyna linijka ktora rozni sie ten branch");
-=======
 Console.WriteLine("Hello, Karol!");
->>>>>>> origin/Mateusz
