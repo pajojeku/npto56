17cc156580694d3b5c8ae934f49d39b310c9c321
pajojeku <pajojeq@gmail.com>
2024-04-15 18:40:04 +0200

    Dodanie programu z poprzednich zajec

---
+++ app/Program.cs
@@ -0,0 +1,20 @@
+﻿
+class Program {
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+
+        int[] tablica = new int[10];
+
+        for (int i = 0; i < tablica.Length; i++) {
+            tablica[i] = random.Next(0, 1001);
+        }
+
+        Console.WriteLine("Liczby parzyste w tablicy:");
+        foreach (int liczba in tablica) {
+            if (liczba % 2 == 0) {
+                Console.WriteLine(liczba);
+            }
+        }
+    }
+}
